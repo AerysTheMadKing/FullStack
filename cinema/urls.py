7d34cf50e438bb7df325c20a76f96e510d6033d7
NuumanIsaev <aestheticdude21@gmail.com>
2023-04-12 11:48:59 +0600

    third commit

--- cinema/urls.py
+++ cinema/urls.py
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/accounts/', include('account.urls')),
+    path('accounts/', include('account.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path('^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
